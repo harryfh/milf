3f1b3a1d52c321c82b05e1c172c1395fc6c607c4
Harry <harryfinkhynes@gmail.com>
2024-09-13 14:07:33 -0400

    Script

--- Milf/js/script.js
+++ Milf/js/script.js
@@ -1,47 +1,49 @@
-const allLinks = document.querySelectorAll("a:link")
-//const mybutton = document.getElementById("goBack")
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    const href = link.getAttribute("href")
-    if (href === "#" || href.startsWith("#")) {
-      e.preventDefault()
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const allLinks = document.querySelectorAll("a:link")
+  //const mybutton = document.getElementById("goBack")
+  allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      const href = link.getAttribute("href")
+      if (href === "#" || href.startsWith("#")) {
+        e.preventDefault()
+      }
 
-    console.log(href)
-    // Scroll To Top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      })
+      console.log(href)
+      // Scroll To Top
+      if (href === "#")
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        })
 
-    // Scroll Else
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionElement = document.querySelector(href)
-      sectionElement.scrollIntoView({ behavior: "smooth" })
-    }
+      // Scroll Else
+      if (href !== "#" && href.startsWith("#")) {
+        const sectionElement = document.querySelector(href)
+        sectionElement.scrollIntoView({ behavior: "smooth" })
+      }
 
-    // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    //     mybutton.style.opacity = "1";
-    // } else {
-    //     mybutton.style.opacity = "0";
-    // }
+      // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      //     mybutton.style.opacity = "1";
+      // } else {
+      //     mybutton.style.opacity = "0";
+      // }
+    })
   })
+
+  function checkFlexGap() {
+    var flex = document.createElement("div")
+    flex.style.display = "flex"
+    flex.style.flexDirection = "column"
+    flex.style.rowGap = "1px"
+
+    flex.appendChild(document.createElement("div"))
+    flex.appendChild(document.createElement("div"))
+
+    document.body.appendChild(flex)
+    var isSupported = flex.scrollHeight === 1
+    document.body.removeChild(flex)
+    console.log("Flex gap supported:", isSupported)
+  }
+
+  checkFlexGap()
 })
-function checkFlexGap() {
-  var flex = document.createElement("div")
-  flex.style.display = "flex"
-  flex.style.flexDirection = "column"
-  flex.style.rowGap = "1px"
-
-  flex.appendChild(document.createElement("div"))
-  flex.appendChild(document.createElement("div"))
-
-  document.body.appendChild(flex)
-  var isSupported = flex.scrollHeight === 1
-  flex.parentNode.removeChild(flex)
-  console.log(isSupported)
-
-  if (!isSupported) document.body.classList.add("no-flexbox-gap")
-}
-checkFlexGap()
