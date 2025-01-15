0500b6c5f839a3c2722a67b5f45cab29f68c3d68
harryfh <harryfinkhynes@gmail.com>
2025-01-15 18:08:13 -0500

    Back to a one page website Rebranded for 2025 Replaced submission with tickets

--- Milf/js/script.js
+++ Milf/js/script.js
@@ -1,24 +1,24 @@
-const allLinks = document.querySelectorAll("a:link")
+const allLinks = document.querySelectorAll("a:link");
 //const mybutton = document.getElementById("goBack")
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    const href = link.getAttribute("href")
+    const href = link.getAttribute("href");
     if (href === "#" || href.startsWith("#")) {
-      e.preventDefault()
+      e.preventDefault();
     }
 
-    console.log(href)
+    console.log(href);
     // Scroll To Top
     if (href === "#")
       window.scrollTo({
         top: 0,
         behavior: "smooth",
-      })
+      });
 
     // Scroll Else
     if (href !== "#" && href.startsWith("#")) {
-      const sectionElement = document.querySelector(href)
-      sectionElement.scrollIntoView({ behavior: "smooth" })
+      const sectionElement = document.querySelector(href);
+      sectionElement.scrollIntoView({ behavior: "smooth" });
     }
 
     // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -26,23 +26,23 @@ allLinks.forEach(function (link) {
     // } else {
     //     mybutton.style.opacity = "0";
     // }
-  })
-})
+  });
+});
 function checkFlexGap() {
-  var flex = document.createElement("div")
-  flex.style.display = "flex"
-  flex.style.flexDirection = "column"
-  flex.style.rowGap = "1px"
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
 
-  flex.appendChild(document.createElement("div"))
-  flex.appendChild(document.createElement("div"))
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
   if (document.body) {
-    document.body.appendChild(flex)
-    var isSupported = flex.scrollHeight === 1
-    flex.parentNode.removeChild(flex)
-    console.log(isSupported)
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
 
-    if (!isSupported) document.body.classList.add("no-flexbox-gap")
+    if (!isSupported) document.body.classList.add("no-flexbox-gap");
   }
 }
-checkFlexGap()
+checkFlexGap();
